883ad7670eda9e1449a510e389a3b28308bc215c
Davi M <davi.aviva@gmail.com>
2017-05-04 21:22:37 -0300

    + coisa

---
+++ mn/rev/interpol_MMQ.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+n = int(input("N: "))
+
+X = np.zeros(n)
+for i in range(1,n+1):
+    X[i-1]=i
+Y = np.zeros(n)
+for i in range(0,n):
+    k = i+1
+    Y[i] = float(input("y%d: " %k))
+
+xm = X.mean()
+ym = Y.mean()
+xym = (X * Y).mean()
+xxm = (X * X).mean()
+yym=(Y*Y).mean()
+am = (xxm * ym - xm *xym) / (xxm - xm * xm)
+bm = (xym - xm * ym) / (xxm - xm * xm)
+
+print("y = %.6f + %.6f * x" % (am,bm))
